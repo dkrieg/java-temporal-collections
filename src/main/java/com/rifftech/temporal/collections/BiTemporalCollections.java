a6200e4fb30f9f531713b78ede04d9b43d802260
c840234 <daniel.krieg@bnsf.com>
2025-02-11 13:58:05 -0600

    Add BiTemporalCollections class with test cases
    Introduced the BiTemporalCollections class for managing bi-temporal collections, including immutable, mutable, and event-publishing types. Added comprehensive unit tests to verify functionality, ensuring proper handling of temporal records in various scenarios.

---
+++ src/main/java/com/rifftech/temporal/collections/BiTemporalCollections.java
@@ -0,0 +1,33 @@
+package com.rifftech.temporal.collections;
+
+import com.rifftech.temporal.events.BiTemporalEventProducer;
+import com.rifftech.temporal.events.TemporalEventProducer;
+
+import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class BiTemporalCollections {
+    private static <T> ConcurrentSkipListBiTemporalCollection<T> createBiTemporalCollection(Collection<BiTemporalRecord<T>> temporalRecords) {
+        SortedSet<BiTemporalRecord<T>> sorted = new TreeSet<>(temporalRecords);
+
+        ConcurrentSkipListBiTemporalCollection<T> collection = new ConcurrentSkipListBiTemporalCollection<>();
+        sorted.forEach(r -> {
+            collection.effectiveAsOf(r.businessEffective().start(), r.systemEffective().start(), r.value());
+            collection.expireAsOf(r.businessEffective().end(), r.systemEffective().end());
+        });
+        return collection;
+    }
+
+    public static <T> BiTemporalCollection<T> immutableBiTemporalCollection(Collection<BiTemporalRecord<T>> temporalRecords) {
+        return new ImmutableBiTemporalCollection<>(createBiTemporalCollection(temporalRecords));
+    }
+
+    public static <T> MutableBiTemporalCollection<T> mutableBiTemporalCollection(Collection<BiTemporalRecord<T>> temporalRecords) {
+        return createBiTemporalCollection(temporalRecords);
+    }
+
+    public static <T> EventPublishingBiTemporalCollection<T> mutableBiTemporalCollection(Collection<BiTemporalRecord<T>> temporalRecords, BiTemporalEventProducer<T> producer) {
+        return new EventPublishingBiTemporalCollection<>(createBiTemporalCollection(temporalRecords), producer);
+    }
+}
